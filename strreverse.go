4c2f124378467c161ee7dbfb601e32e899b8ffe1
swathimanu <svangoor@us.ibm.com>
2015-09-15 22:07:41 -0400

    Initial Commit

---
+++ strreverse.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Reverse(s string) string{
+	rev := []rune(s)
+	for i, j := 0, len(rev)-1; i < len(rev)/2; i,j = i+1, j-1{
+		rev[i], rev[j] = rev[j], rev[i]
+
+	}
+	return string(rev)
+}
+func main() {
+
+	fmt.Println(Reverse("Golang"))
+	
+}
